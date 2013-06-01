6fec34b80cda4a00bc7b4348ed18f858526dc8cb
Hann <jinsu0411@gmail.com>
2013-06-01 15:25:48 +0900

    add Main.js for starting app

---
+++ Main.js
@@ -0,0 +1,9 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: hanjinsoo
+ * Date: 6/1/13
+ * Time: 3:22 오후
+ * To change this template use File | Settings | File Templates.
+ */
+
+http = require('http');
